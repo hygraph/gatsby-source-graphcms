c1137193be41ce002040cc606c23eb36e078e88c
Jonathan Steele <jonathan@steele.pro>
2020-07-07 14:41:53 +0100

    feat: Begin to build Gatsby nodes from remote GraphCMS schema

---
+++ gatsby-source-graphcms/gatsby-node.js
@@ -0,0 +1,28 @@
+const {
+  createDefaultQueryExecutor,
+  loadSchema,
+  generateDefaultFragments,
+} = require('gatsby-graphql-source-toolkit')
+const pluralize = require('pluralize')
+
+const createSourcingConfig = async (gatsbyApi, { endpoint, token }) => {
+  const execute = createDefaultQueryExecutor(endpoint)
+  const schema = await loadSchema(execute)
+
+  const nodeInterface = schema.getType('Node')
+  const possibleTypes = schema.getPossibleTypes(nodeInterface)
+
+  const gatsbyNodeTypes = possibleTypes.map((type) => ({
+    remoteTypeName: type,
+    remoteIdFields: ['__typename', 'id'],
+    queries: `query LIST_${pluralize(
+      type
+    ).toUpperCase()} { products(limit: $limit, skip: $skip) }`,
+  }))
+
+  const fragments = generateDefaultFragments({ schema, gatsbyNodeTypes })
+}
+
+exports.sourceNodes = async (gatsbyApi, pluginOptions) => {
+  const config = await createSourcingConfig(gatsbyApi, pluginOptions)
+}
