e35a988e49913a0a2323d19e64bf28219fddeaef
Jonathan Steele <jonathan@steele.pro>
2020-07-10 13:59:23 +0100

    fix: Correct individual node query

--- gatsby-source-graphcms/gatsby-node.js
+++ gatsby-source-graphcms/gatsby-node.js
@@ -34,7 +34,7 @@ const createSourcingConfig = async (gatsbyApi, { endpoint, token }) => {
       query LIST_${pluralize(type.name).toUpperCase()} { ${pluralize(
       type.name.toLowerCase()
     )}(first: $limit, skip: $offset) }
-      query NODE_${type.name.toUpperCase()} { ${type.name.toLowerCase()}(id: $id) }`,
+      query NODE_${type.name.toUpperCase()}($id: ID!) { ${type.name.toLowerCase()}(where: { id: $id }) }`,
   }))
 
   const fragments = generateDefaultFragments({ schema, gatsbyNodeTypes })
