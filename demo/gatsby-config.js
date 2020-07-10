0e50aae570d27520cd44041243eed460ee8f00c3
Jonathan Steele <jonathan@steele.pro>
2020-07-10 12:16:13 +0100

    refactor: Use dotenv

--- demo/gatsby-config.js
+++ demo/gatsby-config.js
@@ -1,11 +1,13 @@
+require('dotenv').config()
+
 module.exports = {
   plugins: [
     {
       resolve: 'gatsby-source-graphcms',
       options: {
         downloadLocalImages: true,
-        endpoint:
-          'https://api-eu-central-1.graphcms.com/v2/ck8sn5tnf01gc01z89dbc7s0o/master',
+        endpoint: process.env.GRAPHCMS_ENDPOINT,
+        token: process.env.GRAPHCMS_TOKEN,
       },
     },
   ],
