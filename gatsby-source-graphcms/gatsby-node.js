246bc6a225698a7e3e2de6e648cac5fdd09e5b0c
Jamie Barton <jamie@notrab.dev>
2020-07-23 09:06:01 +0100

    fix: don't error when no token provided

--- gatsby-source-graphcms/gatsby-node.js
+++ gatsby-source-graphcms/gatsby-node.js
@@ -7,42 +7,37 @@ const {
   createSchemaCustomization,
   sourceAllNodes,
   sourceNodeChanges,
-} = require('gatsby-graphql-source-toolkit')
-const { createRemoteFileNode } = require('gatsby-source-filesystem')
-const fetch = require('node-fetch')
-const pluralize = require('pluralize')
+} = require("gatsby-graphql-source-toolkit");
+const { createRemoteFileNode } = require("gatsby-source-filesystem");
+const fetch = require("node-fetch");
+const pluralize = require("pluralize");
 
 exports.onPreBootstrap = ({ reporter }, pluginOptions) => {
   if (!pluginOptions || !pluginOptions.endpoint)
     return reporter.panic(
-      'gatsby-source-graphcms: You must provide your GraphCMS endpoint URL'
-    )
-
-  if (!pluginOptions || !pluginOptions.token)
-    return reporter.panic(
-      'gatsby-source-graphcms: You must provide a GraphCMS API token'
-    )
-}
+      "gatsby-source-graphcms: You must provide your GraphCMS endpoint URL"
+    );
+};
 
 const createSourcingConfig = async (gatsbyApi, { endpoint, token }) => {
   const execute = async ({ operationName, query, variables = {} }) => {
     return await fetch(endpoint, {
-      method: 'POST',
+      method: "POST",
       body: JSON.stringify({ query, variables, operationName }),
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
         ...(token && { Authorization: `Bearer ${token}` }),
       },
-    }).then((res) => res.json())
-  }
-  const schema = await loadSchema(execute)
+    }).then((res) => res.json());
+  };
+  const schema = await loadSchema(execute);
 
-  const nodeInterface = schema.getType('Node')
-  const possibleTypes = schema.getPossibleTypes(nodeInterface)
+  const nodeInterface = schema.getType("Node");
+  const possibleTypes = schema.getPossibleTypes(nodeInterface);
 
   const gatsbyNodeTypes = possibleTypes.map((type) => ({
     remoteTypeName: type.name,
-    remoteIdFields: ['__typename', 'id'],
+    remoteIdFields: ["__typename", "id"],
     queries: `
       query LIST_${pluralize(type.name).toUpperCase()} { ${pluralize(
       type.name.toLowerCase()
@@ -51,15 +46,15 @@ const createSourcingConfig = async (gatsbyApi, { endpoint, token }) => {
       type.name
     }WhereUniqueInput!) { ${type.name.toLowerCase()}(where: $where) }`,
     nodeQueryVariables: ({ id }) => ({ where: { id } }),
-  }))
+  }));
 
-  const fragments = generateDefaultFragments({ schema, gatsbyNodeTypes })
+  const fragments = generateDefaultFragments({ schema, gatsbyNodeTypes });
 
   const documents = compileNodeQueries({
     schema,
     gatsbyNodeTypes,
     customFragments: fragments,
-  })
+  });
 
   return {
     gatsbyApi,
@@ -67,46 +62,46 @@ const createSourcingConfig = async (gatsbyApi, { endpoint, token }) => {
     execute: wrapQueryExecutorWithQueue(execute, { concurrency: 10 }),
     gatsbyTypePrefix: `GraphCMS_`,
     gatsbyNodeDefs: buildNodeDefinitions({ gatsbyNodeTypes, documents }),
-  }
-}
+  };
+};
 
 exports.sourceNodes = async (gatsbyApi, pluginOptions) => {
-  const { webhookBody } = gatsbyApi
+  const { webhookBody } = gatsbyApi;
 
-  const config = await createSourcingConfig(gatsbyApi, pluginOptions)
+  const config = await createSourcingConfig(gatsbyApi, pluginOptions);
 
-  await createSchemaCustomization(config)
+  await createSchemaCustomization(config);
 
   if (webhookBody && Object.keys(webhookBody).length) {
-    const { operation, data } = webhookBody
+    const { operation, data } = webhookBody;
 
     const nodeEvent = (operation, { __typename, id }) => {
       switch (operation) {
-        case 'delete':
-        case 'unpublish':
+        case "delete":
+        case "unpublish":
           return {
-            eventName: 'DELETE',
+            eventName: "DELETE",
             remoteTypeName: __typename,
             remoteId: { __typename, id },
-          }
-        case 'create':
-        case 'publish':
-        case 'update':
+          };
+        case "create":
+        case "publish":
+        case "update":
           return {
-            eventName: 'UPDATE',
+            eventName: "UPDATE",
             remoteTypeName: __typename,
             remoteId: { __typename, id },
-          }
+          };
       }
-    }
+    };
 
     await sourceNodeChanges(config, {
       nodeEvents: [nodeEvent(operation, data)],
-    })
+    });
   } else {
-    await sourceAllNodes(config)
+    await sourceAllNodes(config);
   }
-}
+};
 
 exports.onCreateNode = async (
   { node, actions: { createNode }, createNodeId, getCache },
@@ -114,8 +109,8 @@ exports.onCreateNode = async (
 ) => {
   if (
     downloadLocalImages &&
-    node.remoteTypeName === 'Asset' &&
-    node.mimeType.includes('image/')
+    node.remoteTypeName === "Asset" &&
+    node.mimeType.includes("image/")
   ) {
     try {
       const fileNode = await createRemoteFileNode({
@@ -124,14 +119,14 @@ exports.onCreateNode = async (
         createNode,
         createNodeId,
         getCache,
-      })
+      });
 
-      if (fileNode) node.localFile = fileNode.id
+      if (fileNode) node.localFile = fileNode.id;
     } catch (e) {
-      console.error('gatsby-source-graphcms:', e)
+      console.error("gatsby-source-graphcms:", e);
     }
   }
-}
+};
 
 exports.createSchemaCustomization = (
   { actions: { createTypes } },
@@ -142,5 +137,5 @@ exports.createSchemaCustomization = (
     type GraphCMS_Asset {
       localFile: File @link
     }
-  `)
-}
+  `);
+};
