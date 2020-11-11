ef71c18b6897e1d9a60e8696b0e5b010f8b7b3f7
Jonathan Steele <jonathan@steele.pro>
2020-11-11 12:15:21 +0000

    feat: Use new pluginOptionsSchema API
    Requires gatsby@2.25.0, keep old options safeguarding for now

--- gatsby-source-graphcms/src/gatsby-node.js
+++ gatsby-source-graphcms/src/gatsby-node.js
@@ -14,6 +14,46 @@ const { createRemoteFileNode } = require('gatsby-source-filesystem')
 const he = require('he')
 const fetch = require('node-fetch')
 
+
+exports.pluginOptionsSchema = ({ Joi }) => {
+  return Joi.object({
+    buildMarkdownNodes: Joi.boolean()
+      .description(
+        `Build markdown nodes for all [RichText](https://graphcms.com/docs/reference/fields/rich-text) fields in your GraphCMS schema`
+      )
+      .default(false),
+    downloadLocalImages: Joi.boolean()
+      .description(
+        `Download and cache GraphCMS image assets in your Gatsby project`
+      )
+      .default(false),
+    endpoint: Joi.string()
+      .description(
+        `The endpoint URL for the GraphCMS project. This can be found in the [project settings UI](https://graphcms.com/docs/guides/concepts/apis#working-with-apis)`
+      )
+      .required(),
+    fragmentsPath: Joi.string()
+      .description(
+        `The local project path where generated query fragments are saved. This is relative to your current working directory. If using multiple instances of the source plugin, you **must** provide a value here to prevent type and/or fragment conflicts.`
+      )
+      .default(`graphcms-fragments`),
+    locales: Joi.array()
+      .description(
+        `An array of locale key strings from your GraphCMS project. You can read more about working with localisation in GraphCMS [here](https://graphcms.com/docs/guides/concepts/i18n).`
+      )
+      .items(Joi.string())
+      .default(['en']),
+    token: Joi.string().description(
+      `If your GraphCMS project is **not** publicly accessible, you will need to provide a [Permanent Auth Token](https://graphcms.com/docs/reference/authorization) to correctly authorize with the API. You can learn more about creating and managing API tokens [here](https://graphcms.com/docs/guides/concepts/apis#working-with-apis)`
+    ),
+    typePrefix: Joi.string()
+      .description(
+        `The string by which every generated type name is prefixed with. For example, a type of Post in GraphCMS would become GraphCMS_Post by default. If using multiple instances of the source plugin, you **must** provide a value here to prevent type conflicts`
+      )
+      .default(`GraphCMS_`),
+  })
+}
+
 exports.onPreBootstrap = ({ reporter }, pluginOptions) => {
   if (!pluginOptions || !pluginOptions.endpoint)
     return reporter.panic(
