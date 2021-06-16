d71943509e3ad3ca6347ec65e560f6350c480922
Jamie Barton <jamie@notrab.dev>
2021-06-16 16:18:26 +0100

    feat: add concurrenecy override (#201)
    * feat: add concurrenecy override
    
    * feat: rename concurrency config option

--- gatsby-source-graphcms/src/gatsby-node.js
+++ gatsby-source-graphcms/src/gatsby-node.js
@@ -69,12 +69,17 @@ export function pluginOptionsSchema({ Joi }) {
         `The string by which every generated type name is prefixed with. For example, a type of Post in GraphCMS would become GraphCMS_Post by default. If using multiple instances of the source plugin, you **must** provide a value here to prevent type conflicts`
       )
       .default(`GraphCMS_`),
+    concurrency: Joi.number()
+      .integer()
+      .min(1)
+      .default(10)
+      .description(`The number of promises to run at one time.`),
   })
 }
 
 const createSourcingConfig = async (
   gatsbyApi,
-  { endpoint, fragmentsPath, locales, stages, token, typePrefix }
+  { endpoint, fragmentsPath, locales, stages, token, typePrefix, concurrency }
 ) => {
   const execute = async ({ operationName, query, variables = {} }) => {
     const { reporter } = gatsbyApi
@@ -199,7 +204,7 @@ const createSourcingConfig = async (
   return {
     gatsbyApi,
     schema,
-    execute: wrapQueryExecutorWithQueue(execute, { concurrency: 10 }),
+    execute: wrapQueryExecutorWithQueue(execute, { concurrency }),
     gatsbyTypePrefix: typePrefix,
     gatsbyNodeDefs: buildNodeDefinitions({ gatsbyNodeTypes, documents }),
   }
