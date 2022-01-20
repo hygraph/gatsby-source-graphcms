61a95ee5beb6f30e1aeeefd083e095ddc855a6b8
Carsten Meier <c.meier@one-unity.de>
2022-01-20 14:04:36 +0100

    feat: add downloadLocalImages POC

--- demo/gatsby-config.js
+++ demo/gatsby-config.js
@@ -2,10 +2,15 @@ require('dotenv').config()
 
 module.exports = {
   plugins: [
-    'gatsby-plugin-image',
-    'gatsby-plugin-sharp',
     'gatsby-plugin-mdx',
     'gatsby-plugin-postcss',
+    { 
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: "assets",
+        path: `${__dirname}/src/assets`,
+      }
+    },
     {
       resolve: 'gatsby-source-graphcms',
       options: {
@@ -15,7 +20,32 @@ module.exports = {
           'https://api-eu-central-1.graphcms.com/v2/ckclvjtet0f0901z69og3f3gm/master',
         locales: ['en', 'de'],
         stages: ['DRAFT', 'PUBLISHED'],
+        downloadLocalImages: true
       },
     },
+    'gatsby-plugin-image',
+    {
+      resolve: "gatsby-plugin-sharp",
+      options: {
+        // Defaults used for gatsbyImageData and StaticImage
+        defaults: {
+          quality: 100,
+          formats: ["auto", "webp"],
+          placeholder: "blurred",
+          blurredOptions: {
+            width: 80,
+          },
+          webpOptions: {
+            quality: 100,
+            alphaQuality: 100,
+            lossless: true,
+            smartSubsample: true
+          }
+        },
+        // Set to false to allow builds to continue on image errors
+        failOnError: true
+      },
+    },
+    "gatsby-transformer-sharp",
   ],
 }
